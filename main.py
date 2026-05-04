0ae581627593148590c276ade2146dc07ad60d9d
Vishal <kajariavishal5@gmail.com>
2026-05-04 23:21:22 +0530

    Updated project with JSON data, functions and query handling

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-students=[
-    {"id":1,"name":"Alice","marks": 85},
-    {"id":2,"name":"Bob","marks":90},
-    {"id":3,"name":"Charlie","marks":78}
-]
+import json
 
-query = "Show students with marks above 80"
+# Load data from JSON file
+def load_students():
+    with open("data.json", "r") as file:
+        return json.load(file)
 
-context = ""
+# Retrieve students data based on user query
+def retrieve_students(query,students):
+    results = []
+    if "top" in query:
+        top_student = students[0]
+        for s in students:
+            if s["marks"] > top_student["marks"]:
+                top_student = s
+        results.append(top_student)
+    
+    for student in students:
+        if "above" in query and student["marks"] > 80:
+            results.append(student)
+        elif "below" in query and student["marks"] < 70:
+            results.append(student)
 
-for s in students:
-    if s["marks"] > 80:
-        context += f"{s['name']} scored {s['marks']} marks.\n"
+    return results
 
-print("Query:", query)
-print("Context:\n", context)
 
-print("Final Answer:")
-if context:
-    print("Students who scored above 80 are:\n", context)
-else:
-    print("No students found.")
+#Build context for RAG 
+def build_context(results):
+    context = ""
+    for student in results:
+        context += f"Name: {student['name']} scored Marks: {student['marks']}\n"
+    return context
+
+#Generate response based on context
+def generate_response(context):
+    if context:
+        return f"Here are the students matching your query:\n{context}"
+    else:
+        return "No students found matching your query."
+
+# Main function to run the RAG assistant
+def main():
+    students= load_students()
+    query = input("Enter your query (e.g., 'Show students above 80 marks'): ").lower()
+    results = retrieve_students(query,students)
+    context = build_context(results)
+    response = generate_response(context)
+
+    print("Final Response:")
+    print(response)
+
+if __name__ == "__main__":
+    main()
